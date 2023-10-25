2d9a67fff4801ed3b4da37710068db02eca845dd
apple <apple@applede-MacBook-Pro-2.local>
2023-10-26 00:41:47 +0100

    lab3 practice starts

--- api/app.py
+++ api/app.py
@@ -1,20 +1,24 @@
 from flask import Flask, render_template, request
 
-app=Flask(__name__) # __name__ 代表目前執行的模組
+app = Flask(__name__)  # __name__ 代表目前執行的模組
 
-@app.route("/") # 函式的裝飾(Decorator)： 以函式為基礎，提供附加的功能
+
+@app.route("/")  # 函式的裝飾(Decorator)： 以函式為基礎，提供附加的功能
 def home():
     return render_template("index.html")
 
-@app.route("/test") # 當網站連接到根目錄底下的test路徑，執行test函式
+
+@app.route("/test")  # 當網站連接到根目錄底下的test路徑，執行test函式
 def test():
     return "This is test lalala."
 
-@app.route("/submit", methods=["POST"]) 
+
+@app.route("/submit", methods=["POST"])
 def submit():
     input_name = request.form.get("name")
     input_age = request.form.get("age")
     return render_template("hello.html", name=input_name, age=input_age)
+
 
 if __name__ == "__main__":
     app.run(debugger=True)
